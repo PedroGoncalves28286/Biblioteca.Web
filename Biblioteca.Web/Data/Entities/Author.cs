1f948e4a96966198a701be109af1c4b1d1791366
Pedro Goncalves <pedromfonsecagoncalves@gmail.com>
2023-08-04 14:48:34 +0100

    Add Entities and controllers

---
+++ Biblioteca.Web/Data/Entities/Author.cs
@@ -0,0 +1,12 @@
+﻿namespace Biblioteca.Web.Data.Entities
+{
+    public class Author
+    {
+
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
---
+++ Biblioteca.Web/Data/Entities/Rental.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.Eventing.Reader;
+using System.Xml.Linq;
+
+namespace Biblioteca.Web.Data.Entities
+{
+    public class Rental
+    {
+        public int Id { get; set; }
+
+        public string UserId { get; set; }
+
+        public string Author { get; set; }
+
+        public string Title { get; set; } 
+
+        public int BookId { get; set; }
+
+        [Display(Name = "Cover")]
+
+        public string ImageUrl { get; set; }
+
+        public string Availability { get; set; }
+
+        public string ISBN { get; set; }
+
+        public string Publisher { get; set; }
+
+        public DateTime? StartDate { get; set; }
+
+        public DateTime? ScheduleReturnDate { get; set; }
+
+        public DateTime? ActualReturnDate { get; set; }   
+
+        public int RentalDuration { get; set; } 
+
+     
+
+
+        public enum StatusEnum
+        {
+            Requested,
+            Approved,
+            Rejected,
+            Rented,
+            Closed
+
+        }
+
+
+    }
+}
