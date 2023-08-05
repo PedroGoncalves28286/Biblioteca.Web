448f8b7a32e8da3cd3952a52d5cc44071baf30fe
eduardotarik <eduardotarik@outlook.pt>
2023-08-05 20:09:18 +0100

    Genre Repository

--- Biblioteca.Web/Data/GenreRepository.cs
+++ Biblioteca.Web/Data/GenreRepository.cs
@@ -1,6 +1,14 @@
-﻿namespace Biblioteca.Web.Data
+﻿using Biblioteca.Web.Data.Entities;
+
+namespace Biblioteca.Web.Data
 {
-    public class GenreRepository
+    public class GenreRepository : GenericRepository<Genre>, IGenreRepository
     {
+        private readonly DataContext _context;
+
+        public GenreRepository(DataContext context) : base(context)
+        {
+            _context = context;
+        }
     }
 }
