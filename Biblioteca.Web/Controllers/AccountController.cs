7b9574505e5da0b872e6615016ec57e9e6452082
Pedro Goncalves <pedromfonsecagoncalves@gmail.com>
2023-08-14 11:28:59 +0100

    Change User Profile and password

--- Biblioteca.Web/Controllers/AccountController.cs
+++ Biblioteca.Web/Controllers/AccountController.cs
@@ -46,7 +46,7 @@ namespace Biblioteca.Web.Controllers
                 }
             }
 
-            this.ModelState.AddModelError(string.Empty, "Failed to login");
+            this.ModelState.AddModelError(string.Empty, "Failed to login!");
             return View(model);
         }
         public async Task<IActionResult> Logout()
@@ -99,8 +99,60 @@ namespace Biblioteca.Web.Controllers
                     ModelState.AddModelError(string.Empty, "The user couldn't be logged.");
                 }
             }
+            return View(model);
+        }
+       
+
+        public async Task<IActionResult> ChangeUser()
+        {
+
+            var user = await _userHelper.GetUserByEmailAsync(this.User.Identity.Name);
+
+            var model = new ChangeUserViewModel();
+
+            if (user != null)
+
+            {
+
+                model.FirstName = user.FirstName;
+
+                model.LastName = user.LastName;
+
+            }
 
             return View(model);
+
+        }
+        public IActionResult ChangePassword()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ChangePassword(ChangePasswordViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userHelper.GetUserByEmailAsync(this.User.Identity.Name);
+                if (user != null)
+                {
+                    var result = await _userHelper.ChangePasswordAsync(user, model.OldPassword, model.NewPassword);
+                    if (result.Succeeded)
+                    {
+                        return this.RedirectToAction("ChangeUser");
+                    }
+                    else
+                    {
+                        this.ModelState.AddModelError(string.Empty, result.Errors.FirstOrDefault().Description);
+                    }
+                }
+                else
+                {
+                    this.ModelState.AddModelError(string.Empty, "User not found.");
+                }
+            }
+
+            return this.View(model);
         }
     }
 }
--- Biblioteca.Web/Helpers/IUserHelper.cs
+++ Biblioteca.Web/Helpers/IUserHelper.cs
@@ -1,7 +1,9 @@
 ﻿using Biblioteca.Web.Data.Entities;
 using Biblioteca.Web.Models;
 using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Biblioteca.Web.Helpers
 {
@@ -11,5 +13,9 @@ namespace Biblioteca.Web.Helpers
         Task<IdentityResult> AddUserAsync(User user, string password);
         Task<SignInResult> LoginAsync(LoginViewModel model);
         Task LogoutAsync();
+
+        Task<IdentityResult> UpdateUserAsync(User user);
+
+        Task<IdentityResult> ChangePasswordAsync(User user, string oldPassword, string newPassword);
     }
 }
