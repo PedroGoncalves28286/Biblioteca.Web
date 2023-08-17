8dc62282816bfab3bef887c8a8711501d25e4441
JulioSCaseiro <caseiroinc@gmail.com>
2023-08-17 10:09:07 +0100

    Pages 403 & 404

--- Biblioteca.Web/Startup.cs
+++ Biblioteca.Web/Startup.cs
@@ -78,6 +78,11 @@ namespace Biblioteca.Web
             services.AddScoped<INewsletterRepository, NewsletterRepository>();
             services.AddScoped<IRentalRepository, RentalRepository>();
             services.AddScoped<IReservationRepository, ReservationRepository>();
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.LoginPath = "/Account/NotAuthorized";
+                options.AccessDeniedPath = "/Account/NotAuthorized";
+            });
           
         }
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -93,6 +98,8 @@ namespace Biblioteca.Web
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseStatusCodePagesWithReExecute("/error/{0}");
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
